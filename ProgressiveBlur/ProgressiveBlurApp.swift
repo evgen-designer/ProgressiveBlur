3680a5c1fd728f4de7c4d3def28f2de6dcdef0ab
Evgen Designer <ievgen.project@gmail.com>
2025-05-10 08:48:47 +0100

    Initial Commit

---
+++ ProgressiveBlur/ProgressiveBlurApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressiveBlurApp.swift
+//  ProgressiveBlur
+//
+//  Created by Mac on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProgressiveBlurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
